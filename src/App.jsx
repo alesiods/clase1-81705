ff252a7ee470cbab82e1e508c472a4f0911a7e29
alesiods <alesioadrover@gmail.com>
2025-09-02 06:56:31 -0300

    Se agrega detalle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer"
 import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 import NavBar from "./componentes/NavBar/NavBar"
 
@@ -12,7 +13,7 @@ const App = () => {
     <>
       <NavBar/>
       <ItemListContainer/>
-      
+      <ItemDetailContainer/>
     </>
     
    
---
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,15 @@
+import "./ItemDetail.css"
+
+const ItemDetail = ({id, nombre, precio, img}) => {
+  return (
+    <div className="contenedorItem">
+        <h2>Nombre: {nombre}</h2>
+        <h3>Precio: {precio}</h3>
+        <h3>ID: {id}</h3>
+        <img src={img} alt={nombre} />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sit exercitationem ab non perspiciatis reprehenderit facere! Quae perspiciatis laudantium officia? Minus, facilis molestias? Ex et eos nisi illum alias suscipit!</p>
+    </div>
+  )
+}
+
+export default ItemDetail
